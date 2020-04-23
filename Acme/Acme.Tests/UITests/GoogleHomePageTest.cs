5b039fd15df49609b4c761b6fc01e5e329c4b03b
1 <1@X1>
2020-04-23 20:24:15 +0300

    ignored

--- Acme/Acme.Tests/UITests/GoogleHomePageTest.cs
+++ Acme/Acme.Tests/UITests/GoogleHomePageTest.cs
@@ -16,6 +16,7 @@ namespace Acme.Test.UITests
         }
 
         //[Test]
+        [Ignore("")]
         public void GoogleHomeTest()
         {
             GooglePages.HomePage.EnterSearchText("softserve");
