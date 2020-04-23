0220525a837974640a8eb66e940eed73800c26b0
1 <1@X1>
2020-04-23 20:22:43 +0300

    Disabled ui test

--- Acme/Acme.Tests/UITests/GoogleHomePageTest.cs
+++ Acme/Acme.Tests/UITests/GoogleHomePageTest.cs
@@ -4,10 +4,10 @@ using System.Threading;
 
 namespace Acme.Test.UITests
 {
-    [TestFixture]
+   // [TestFixture]
     public class GoogleHomePageTest
     {
-        [SetUp]
+        //[SetUp]
         public void SetUpTest()
         {
             DriverManager.Current = Driver.GetForRemout();
@@ -15,7 +15,7 @@ namespace Acme.Test.UITests
             DriverManager.Current.MaximizeWindow();
         }
 
-        [Test]
+        //[Test]
         public void GoogleHomeTest()
         {
             GooglePages.HomePage.EnterSearchText("softserve");
@@ -23,7 +23,7 @@ namespace Acme.Test.UITests
             GooglePages.HomePage.ClickSearchInGoogleInput();
         }
 
-        [TearDown]
+        //[TearDown]
         public void TearDownTest()
         {
             DriverManager.Current.Quit();
